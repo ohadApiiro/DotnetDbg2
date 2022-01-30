efc1caec345bb8c302f500b35e95c056216dce36
ohadApiiro <ohad@apiiro.com>
2022-01-30 20:08:26 +0200

    first

---
+++ WebApp/Controllers/WeatherForecastController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers;
+
+public class AccessTokenScopeAttribute : AuthorizeAttribute
+{
+    public HashSet<string> Scopes { get; set; }
+
+    public AccessTokenScopeAttribute(string scope) : base()
+        => Scopes = new HashSet<string> {scope};
+}
+
+[ApiController]
+
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    [AccessTokenScope("")]
+    [HttpPost("api")]
+    public void Api()
+    {
+    }
+ 
+    // [AuthorizeAttribute("")]
+    // [HttpPost("api2")]
+    // public void Api2()
+    // {
+    // }
+}
